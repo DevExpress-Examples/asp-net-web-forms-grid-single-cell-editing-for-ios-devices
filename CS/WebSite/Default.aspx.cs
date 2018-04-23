7aaea81957acdde9e09b5f2c3366fdf1f994f14e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿// Developer Express Code Central Example:
+// How to implement a single cell editing feature in ASPxGridView for iOS devices
+// 
+// If you take a look at the http://www.devexpress.com/scid=E430 example, you will
+// see that this feature doesn't work correctly on iOS devices. The keyboard
+// appears and cells can be edited only after you click a cell twice. Editors are
+// created dynamically on a GridView callback and iOS prevents setting focus.
+// Therefore, the keyboard doesn't appear since a cell editor does not exist when a
+// user taps the cell.
+// In this example, we provide a workaround that implements
+// correct behavior. The iOS keyboard is shown if a user taps on an HTML input
+// element and focus is not changed. The main idea is to manipulate the editor's
+// border-width style on the client side by handling the editor's client-side
+// GotFocus and LostFocus events to imitate the default and edit cell states. In
+// this example, data is instantly updated after leaving a cell. Please note that
+// before sending a callback, the editor's value is checked via client-side
+// validation, so that there are no update errors.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E4600
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxGridView;
+using System.Web.UI.HtmlControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void EditorInit(object sender, EventArgs e)
+    {
+        ASPxTextBox tb = sender as ASPxTextBox;
+        GridViewDataItemTemplateContainer container = tb.NamingContainer as GridViewDataItemTemplateContainer;
+        int visibleIndex = container.VisibleIndex;
+        tb.ID += visibleIndex.ToString();
+        tb.ClientInstanceName += visibleIndex.ToString();
+        tb.ClientSideEvents.GotFocus = String.Format("function (s, e) {{ onCellFocus(s, e); }}", visibleIndex);
+        tb.ClientSideEvents.LostFocus = String.Format("function (s, e) {{ onCellLostFocus(s, e, {0}); }}", visibleIndex);
+    }
+
+    protected void cb_CallBack(object source, DevExpress.Web.ASPxCallback.CallbackEventArgs e)
+    {
+        if (String.IsNullOrEmpty(e.Parameter)) return;
+        int visibleIndex = Convert.ToInt32(e.Parameter); 
+
+        //---- The code below cofigures update parameters and write changes to database
+
+        //foreach (GridViewDataColumn col in GridView.Columns)
+        //{
+        //    if (col.FieldName != GridView.KeyFieldName)
+        //    {
+        //        ASPxTextBox tb = GridView.FindRowCellTemplateControl(visibleIndex, col, col.FieldName + visibleIndex.ToString()) as ASPxTextBox;
+        //        SqlDataSource1.UpdateParameters[col.FieldName].DefaultValue = tb.Text;
+               
+        //    }
+                    
+        //}
+        //string key = GridView.GetRowValues(visibleIndex, GridView.KeyFieldName).ToString();
+        //SqlDataSource1.UpdateParameters[GridView.KeyFieldName].DefaultValue = key;
+        //SqlDataSource1.Update();
+    }
+}
---
+++ VB/WebSite/Properties/AssemblyInfo.vb
@@ -0,0 +1,58 @@
+﻿' Developer Express Code Central Example:
+' How to implement a single cell editing feature in ASPxGridView for iOS devices
+' 
+' If you take a look at the http://www.devexpress.com/scid=E430 example, you will
+' see that this feature doesn't work correctly on iOS devices. The keyboard
+' appears and cells can be edited only after you click a cell twice. Editors are
+' created dynamically on a GridView callback and iOS prevents setting focus.
+' Therefore, the keyboard doesn't appear since a cell editor does not exist when a
+' user taps the cell.
+' In this example, we provide a workaround that implements
+' correct behavior. The iOS keyboard is shown if a user taps on an HTML input
+' element and focus is not changed. The main idea is to manipulate the editor's
+' border-width style on the client side by handling the editor's client-side
+' GotFocus and LostFocus events to imitate the default and edit cell states. In
+' this example, data is instantly updated after leaving a cell. Please note that
+' before sending a callback, the editor's value is checked via client-side
+' validation, so that there are no update errors.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4600
+
+
+Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("MutiRowEditingIPAD")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MutiRowEditingIPAD")>
+<Assembly: AssemblyCopyright("Copyright ? 2013")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("15b0727e-133e-4b37-a49d-5aa5d2edabd2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
